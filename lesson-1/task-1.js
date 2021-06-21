9d9fa60789643d18e22f82de1711649390507b27
Андрей Громов <D:\Program Files (x86)\The Bat!\Mail>
2021-06-21 18:37:39 +0300

    lesson 1

---
+++ lesson-1/task-1.js
@@ -0,0 +1,4 @@
+'use sctrict';
+
+const temperature = +prompt('Введите температуру в градусах по Цельсию');
+alert(`Задание 1\nТемпература по Фаренгейту: ${(9 / 5) * temperature + 32}`);
---
+++ lesson-1/task-3.js
@@ -0,0 +1,5 @@
+'use sctrict';
+
+const val = 1000 + '108';
+
+alert(`Задание 3\n${val}`);
